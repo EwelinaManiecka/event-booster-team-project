305f6b3eb06c3b0c227cb4a34ef996ee7a11276f
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-11-18 18:18:53 +0100

    pagination logic

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,34 @@ let buttonHolderID = 'gallery-pagination';
 let itemHolderID = 'gallery-list';
 let pagination = null;
 let itemsOnPage = null;
+let currentName = 'utah';
+let currentCountry = null;
 
-fetchEvents().then(({ events, pageInfo }) => {
-  itemsOnPage = mapItems(events);
-  rednerItems(itemsOnPage, itemHolderID);
-  const { number, totalElemnts, size } = pageInfo;
+const startFetching = isNew => {
+  fetchEvents(currentName, actualPage, currentCountry)
+    .then(({ events, pageInfo }) => {
+      itemsOnPage = mapItems(events);
+      rednerItems(itemsOnPage, itemHolderID);
+      const { number, totalElements, size } = pageInfo;
 
-  actualPage = number;
-  totalItems = totalElemnts;
-  itemsPerPage = size;
+      actualPage = number;
+      totalItems = totalElements;
+      itemsPerPage = size;
+    })
+    .then(() => {
+      if (isNew) {
+        pagination = new Pagination(totalItems, itemsPerPage, buttonHolderID);
+        pagination.create();
+        pagination.handler.on('beforeMove', function (eventData) {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+        pagination.handler.on('afterMove', function (eventData) {
+          actualPage = eventData.page;
+          console.log(actualPage);
+          startFetching(false);
+        });
+      }
+    });
+};
 
-  pagination = new Pagination(totalItems, itemsPerPage, buttonHolderID);
-  pagination.create();
-});
+startFetching(true);
